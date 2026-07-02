b93c1953d9b7a864831d3ca524e3233c4baea8aa
Balaganesh S <balaganesh@juspay.in>
2026-07-02 16:25:29 +0530

    Updated Package for version 2.2.8.1

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "HyperPasskeys",
-            url: "https://public.releases.juspay.in/release/ios/hyper-sdk/2.2.8/HyperPasskeys.zip",
-            checksum: "46107ddf2d9e410783c5f670c11b4c190946ae3a32276096e1ef83ad1024acb3"
+            url: "https://public.releases.juspay.in/release/ios/hyper-sdk/2.2.8.1/HyperPasskeys.zip",
+            checksum: "4c295772441e926adee40ca1f997640448f30da4e39af0ec34277bdce76a2045"
         )
     ]
 )
